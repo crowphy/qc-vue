af818d2231f49d9e535060d4c8c13ec90002054a
吴亚辉(平安官网) <wuyahui065@pingan.com.cn>
2018-02-02 22:26:46 +0800

    front

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
-const Koa = require("koa");
-const Router = require("koa-router");
+const Koa = require('koa');
+const Router = require('koa-router');
+const cors = require('koa2-cors');
+const bodyParser = require('koa-bodyparser');
+const fs = require('fs');
+
 const app = new Koa();
-
 const router = new Router();
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+// bodyParser须放在解析路由之前，否则无法解析
+app.use(bodyParser());
+app.use(cors({
+  allowMethods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept']
+}));
+app.use(router.routes()).use(router.allowedMethods());
 
-router.get('/', (ctx, next) => {
-  // console.log(ctx.req)
-  ctx.body = "hello world";
+
+router.post('/collect', (ctx, next) => {
+  console.log(ctx.request.body);
+  ctx.message = 'ok';
+  ctx.body = null;
 });
 
 app.listen(3000);
